bb5aff6af53e9a264189e21e158747c4d54c8c03
glhr <grenblot@gmail.com>
2020-10-04 18:35:13 +0200

    added pifpaf line strippsss

--- src/openpifpaf/openpifpaf_ros.py
+++ src/openpifpaf/openpifpaf_ros.py
@@ -19,6 +19,7 @@ import rospy
 
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Point
 from visualization_msgs.msg import Marker
 from visualization_msgs.msg import MarkerArray
 from vision_utils.img import numpy_to_image, load_image
@@ -98,9 +99,9 @@ args = parser.parse_args()
 img_path = "/home/robotlab/pose test input/wrist_cam_1600951547.png"
 
 pairs = dict(list(enumerate(openpifpaf.datasets.constants.COCO_KEYPOINTS)))
-print(pairs)
+pp.pprint(pairs)
 
-skel_pub = rospy.Publisher('openpifpaf_skeleton', MarkerArray, queue_size=100)
+skel_pub = rospy.Publisher('openpifpaf_skeleton', Marker, queue_size=100)
 img_pub = rospy.Publisher('openpifpaf_img', Image, queue_size=10)
 rospy.init_node('openpifpaf')
 
@@ -108,48 +109,83 @@ colors = dict()
 for k in range(10):
   colors[k] = tuple(np.random.randint(256, size=3)/256)
 
+connected_points = [
+(0,2), (2,4), (4,6), (6,8), (8,10),
+(0,1), (1,3), (3,5), (5,7), (7,9),
+(5,11), (11,13), (13,15),
+(6,12), (12,14), (14,16),
+(11,12), (5,6)]
+
 # rosrun tf static_transform_publisher 0 0 0 0 0 0 1 map my_frame 10
 while not rospy.is_shutdown():
 
     for img_path in glob.glob(f"{args.input_dir}/*.png"):
+        # img_path = "/home/robotlab/pose test input/wrist_cam_1600951547.png"
         predictions, im, time = predict(img_path, scale=0.5)
         predictions = [ann.json_data() for ann in predictions[0]]
         img_pub.publish(numpy_to_image(im))
 
-        markerArray = MarkerArray()
         for person_id, person in enumerate(predictions):
 
             pnts_openpifpaf = person['keypoints']
             pnts_openpifpaf = list(zip(pnts_openpifpaf[0::3], pnts_openpifpaf[1::3]))
             #d = dict(zip(keys, values))
             pnts_dict = dict()
-            for i,pnt in enumerate(pnts_openpifpaf):
-                pnts_dict[pairs[i]] = pnt
 
-                if pnt[0] > 0 and pnt[1] > 0:
-                    marker = Marker()
-                    marker.header.frame_id = "/map"
-                    marker.type = marker.SPHERE
-                    marker.action = marker.ADD
-                    marker.scale.x = 0.1
-                    marker.scale.y = 0.1
-                    marker.scale.z = 0.1
-                    marker.color.a = 1.0
-                    marker.color.r, marker.color.g, marker.color.b = colors[person_id]
-                    marker.pose.orientation.w = 1.0
-                    marker.pose.position.x = pnt[0]/100
-                    marker.pose.position.y = pnt[1]/100
-                    marker.pose.position.z = 1
-                    marker.id = i + person_id*10
-                    marker.lifetime = rospy.Duration(0.5)
-                    marker.header.stamp = rospy.get_rostime()
-                    markerArray.markers.append(marker)
-                    # pp.pprint([m.id for m in markerArray.markers])
+            for i,conn in enumerate(connected_points):
+                pnt_1 = pnts_openpifpaf[conn[0]]
+                pnt_2 = pnts_openpifpaf[conn[1]]
+                if pnt_1[0] > 0 and pnt_1[1] > 0 and pnt_2[0] > 0 and pnt_2[1] > 0:
+                    time = rospy.get_rostime()
+                    line_marker = Marker()
+                    line_marker.header.frame_id = "/map"
+                    line_marker.type = line_marker.LINE_STRIP
+                    line_marker.action = line_marker.ADD
+                    line_marker.scale.x, line_marker.scale.y, line_marker.scale.z = 0.05, 0.05, 0.05
+                    line_marker.color.a = 1.0
+                    line_marker.color.r, line_marker.color.g, line_marker.color.b = colors[person_id]
+                    line_marker.pose.orientation.w = 1.0
+                    line_marker.pose.position.x = 0
+                    line_marker.pose.position.y = 0
+                    line_marker.pose.position.z = 1
+                    line_marker.id = person_id*100 + i*2+1
+                    line_marker.lifetime = rospy.Duration(1)
+                    line_marker.header.stamp = time
+                    line_marker.points = []
+                    # first point
+                    first_line_point = Point()
+                    first_line_point.x = pnt_1[0]/100
+                    first_line_point.y = pnt_1[1]/100
+                    first_line_point.z = 0.0
+                    line_marker.points.append(first_line_point)
+                    # second point
+                    second_line_point = Point()
+                    second_line_point.x = pnt_2[0]/100
+                    second_line_point.y = pnt_2[1]/100
+                    second_line_point.z = 0.0
+                    line_marker.points.append(second_line_point)
 
+                    # pp.pprint(marker.points)
 
-        skel_pub.publish(markerArray)
-            # pp.pprint(markerArray.markers)
-            # pp.pprint(pnts_dict)
+                    skel_pub.publish(line_marker)
+
+                    pnt_marker = Marker()
+                    pnt_marker.header.frame_id = "/map"
+                    pnt_marker.type = pnt_marker.SPHERE
+                    pnt_marker.action = pnt_marker.ADD
+                    pnt_marker.scale.x, pnt_marker.scale.y, pnt_marker.scale.z = 0.1, 0.1, 0.1
+                    pnt_marker.color.a = 1.0
+                    pnt_marker.color.r, pnt_marker.color.g, pnt_marker.color.b = (1.0,1.0,1.0)
+                    pnt_marker.pose.orientation.w = 1.0
+                    pnt_marker.pose.position.x = pnt_1[0]/100
+                    pnt_marker.pose.position.y = pnt_1[1]/100
+                    pnt_marker.pose.position.z = 1
+                    pnt_marker.id = person_id*100 + i*2
+                    pnt_marker.lifetime = rospy.Duration(1)
+                    pnt_marker.header.stamp = time
+                    skel_pub.publish(pnt_marker)
+        # pp.pprint(markerArray.markers)
+        # pp.pprint(pnts_dict)
 
 # while True:
 #     predictions, time = predict(img_path, scale=0.5)
