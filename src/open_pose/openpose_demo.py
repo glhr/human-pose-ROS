c77a5635ddc6c218dc185304fe1892976e6b0faf
glhr <grenblot@gmail.com>
2020-10-09 16:50:12 +0200

    fixed openpose eval (again)

--- src/open_pose/openpose_demo.py
+++ src/open_pose/openpose_demo.py
@@ -18,76 +18,68 @@ import json
 
 def run_openpose(img_path="/home/slave/Downloads/trump.jpg", scale=1):
     img_name = f'{img_path.split("/")[-1].split(".")[-2]}-{scale}.{img_path.split(".")[-1]}' if scale<1 else img_path.split("/")[-1]
+    # Import Openpose (Windows/Ubuntu/OSX)
+    dir_path = os.path.dirname(os.path.realpath(__file__))
     try:
-        # Import Openpose (Windows/Ubuntu/OSX)
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        try:
-            import pyopenpose as op
-        except ImportError as e:
-            print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
-            raise e
+        import pyopenpose as op
+    except ImportError as e:
+        print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
+        raise e
 
-        # Flags
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--image_path", default=img_path, help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
-        args = parser.parse_known_args()
+    # Flags
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_path", default=img_path, help="Process an image. Read all standard formats (jpg, png, bmp, etc.).")
+    args = parser.parse_known_args()
 
-        # Custom Params (refer to include/openpose/flags.hpp for more parameters)
-        params = dict()
-        params["model_folder"] = "/home/slave/openpose/models/"
-        # params["number_people_max"] = 1
-        print(params)
+    # Custom Params (refer to include/openpose/flags.hpp for more parameters)
+    params = dict()
+    params["model_folder"] = "/home/slave/openpose/models/"
+    # params["number_people_max"] = 1
+    print(params)
 
-        # Add others in path?
-        for i in range(0, len(args[1])):
-            curr_item = args[1][i]
-            if i != len(args[1])-1: next_item = args[1][i+1]
-            else: next_item = "1"
-            if "--" in curr_item and "--" in next_item:
-                key = curr_item.replace('-','')
-                if key not in params:  params[key] = "1"
-            elif "--" in curr_item and "--" not in next_item:
-                key = curr_item.replace('-','')
-                if key not in params: params[key] = next_item
+    # Add others in path?
+    for i in range(0, len(args[1])):
+        curr_item = args[1][i]
+        if i != len(args[1])-1: next_item = args[1][i+1]
+        else: next_item = "1"
+        if "--" in curr_item and "--" in next_item:
+            key = curr_item.replace('-','')
+            if key not in params:  params[key] = "1"
+        elif "--" in curr_item and "--" not in next_item:
+            key = curr_item.replace('-','')
+            if key not in params: params[key] = next_item
 
-        # Construct it from system arguments
-        # op.init_argv(args[1])
-        # oppython = op.OpenposePython()
-        with CodeTimer() as timer:
-            # Starting OpenPose
-            opWrapper = op.WrapperPython()
-            opWrapper.configure(params)
-            opWrapper.start()
+    # Construct it from system arguments
+    # op.init_argv(args[1])
+    # oppython = op.OpenposePython()
+    with CodeTimer() as timer:
+        # Starting OpenPose
+        opWrapper = op.WrapperPython()
+        opWrapper.configure(params)
+        opWrapper.start()
 
-            # Process Image
-            datum = op.Datum()
-            imageToProcess = cv2.imread(args[0].image_path)
-            dim = (int(imageToProcess.shape[1] * scale), int(imageToProcess.shape[0] * scale))
-            imageToProcess = cv2.resize(imageToProcess, dim)
-            datum.cvInputData = imageToProcess
-            opWrapper.emplaceAndPop([datum])
-        print(img_name, timer.took)
+        # Process Image
+        datum = op.Datum()
+        imageToProcess = cv2.imread(args[0].image_path)
+        dim = (int(imageToProcess.shape[1] * scale), int(imageToProcess.shape[0] * scale))
+        imageToProcess = cv2.resize(imageToProcess, dim)
+        datum.cvInputData = imageToProcess
+        opWrapper.emplaceAndPop([datum])
+    print(img_name, timer.took)
 
-        # Display Image
-        json_out = []
-        print("Body keypoints: \n" + str(datum.poseKeypoints))
-        for person in list(datum.poseKeypoints):
-            keypoints = []
-            for kp in person:
-                keypoints.extend([float(p) for p in kp])
-            logger.debug(keypoints)
-            json_out.append({'keypoints':keypoints})
-        json_out_name = '../eval/openpose/' + img_name + '.predictions.json'
-        with open(json_out_name, 'w') as f:
-            json.dump(json_out, f)
-        logger.info(json_out_name)
-        save_image(bgr_to_rgb(datum.cvOutputData), img_name)
-        return timer.took
-        # cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
-        # cv2.waitKey(0)
-    except Exception as e:
-        print(e)
-        sys.exit(-1)
+    # Display Image
+    json_out = []
+    print("Body keypoints: \n" + str(datum.poseKeypoints))
+    keypoints = datum.poseKeypoints.flatten().tolist()
+    json_out.append({'keypoints':keypoints})        
+    json_out_name = '../eval/openpose/' + img_name + '.predictions.json'
+    with open(json_out_name, 'w') as f:
+        json.dump(json_out, f)
+    logger.info(json_out_name)
+    save_image(bgr_to_rgb(datum.cvOutputData), img_name)
+    return timer.took
+    # cv2.imshow("OpenPose 1.6.0 - Tutorial Python API", datum.cvOutputData)
+    # cv2.waitKey(0)
 
 if __name__ == "__main__":
 
