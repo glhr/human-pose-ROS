14e3dc909fbafa5a2785d5583903e298162d7691
glhr <grenblot@gmail.com>
2020-11-17 20:42:07 +0100

    cleaned up and renamed get_centroid

--- src/pose_utils/get_ref_angle.py
+++ src/pose_utils/get_ref_angle.py
@@ -35,37 +35,15 @@ def skel_cb(msg):
     centroids = dict()
     distances = dict()
 
+    msg_dict = message_converter.convert_ros_message_to_dictionary(msg.skeletons[0])
 
-    for skeleton_i, skeleton in enumerate(msg.skeletons):
-        skel_filtered = dict()
-        msg_dict = message_converter.convert_ros_message_to_dictionary(skeleton)
+    centroid = msg_dict["centroid"][0:3]
+    uncertainty = msg_dict["centroid"][3:6]
 
-        msg_dict.pop("id",None)
-        msg_dict.pop("dummy",None)
+    if np.sum(np.array(uncertainty)*uncertainty_gain) < 1000:
+        pan_angle = angle_from_centroid(centroid, ref_vector=[0,1,0], normal_vector=[0,0,-1])
 
-        # print(list(msg_dict_tf.values()))
-
-        # valid_points = [v for v in msg_dict.values() if len(v)]
-
-        if len(msg_dict["centroid"]):
-            # centroids[skeleton_i] = get_points_centroid(list(valid_points))
-            centroids[skeleton_i] = msg_dict["centroid"][0:3]
-            #
-            # if args.debug: logger.debug("{} - Centroid: {}".format(skeleton_i, centroids[skeleton_i] ))
-            # skeleton.centroid = centroids[skeleton_i]
-
-            # if len(centroids[skeleton_i]):
-            distances[skeleton_i] = distance_between_points([0,0,0],centroids[skeleton_i])
-        else:
-            distances[skeleton_i] = -1
-    uncertain = msg_dict["centroid"][3:6]
-
-    if np.sum(np.array(msg_dict["centroid"][3:6])*uncertainty_gain) < 1000:
-        closest_skeleton_i = max(0,min(distances, key=distances.get))
-        msg.tracked_person_id = closest_skeleton_i
-        pan_angle = angle_from_centroid(centroids[closest_skeleton_i], ref_vector=[0,1,0], normal_vector=[0,0,-1])
-
-        centroid_v = vector_from_2_points(camera_point,centroids[closest_skeleton_i])
+        centroid_v = vector_from_2_points(camera_point,centroid)
         tilt_angle = angle_from_centroid(centroid_v, ref_vector=ref_v, normal_vector=[1,0,0])
         if args.debug: logger.debug("--> Angle of closest person {}: pan {} tilt {}".format(closest_skeleton_i, pan_angle, tilt_angle))
         pan_pub.publish(pan_angle)
@@ -74,12 +52,9 @@ def skel_cb(msg):
         pan_pub.publish(200)
         tilt_pub.publish(200)
 
-    pose_pub.publish(msg)
-
 rospy.init_node("get_centroid")
 
-rospy.Subscriber("ros_transformed_openpifpaf_pose_kalman",PoseEstimation,skel_cb)
-pose_pub = rospy.Publisher("kalman_pose_full",PoseEstimation, queue_size=1)
+rospy.Subscriber("openpifpaf_pose_transformed_kalman",PoseEstimation,skel_cb)
 pan_pub = rospy.Publisher('ref_pan_angle', Float32, queue_size=1)
 tilt_pub = rospy.Publisher('ref_tilt_angle', Float32, queue_size=1)
 
