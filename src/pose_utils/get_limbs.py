7fe6faef58e55b1554f54e8e4213d4d9d1c0c97b
glhr <grenblot@gmail.com>
2020-11-23 14:42:45 +0100

    improving limb constraints

--- src/pose_utils/get_limbs.py
+++ src/pose_utils/get_limbs.py
@@ -11,13 +11,39 @@ from vision_utils.timing import get_timestamp
 
 from visualization_msgs.msg import Marker, MarkerArray
 from geometry_msgs.msg import Point
+from sensor_msgs.msg import Image, CameraInfo
 from pose_utils.utils import *
+
+from sympy.solvers import solve
+from sympy import Symbol
+
 
 logger=get_logger()
 pp = get_printer()
 
 cam = True
 FRAME_ID = "/world"
+
+rospy.init_node('pose_limbs')
+
+import argparse
+parser = argparse.ArgumentParser(description='Visualization options')
+parser.add_argument('--lifetime',
+                 default=1,
+                 help='Marker lifetime')
+parser.add_argument('--filter',
+                 action='store_true',
+                 help='Use filtered poses')
+parser.add_argument('--debug',
+                 action='store_true',
+                 help='Print visualization debug')
+parser.add_argument('--cam',
+                 default='wrist')
+
+args, unknown = parser.parse_known_args()
+
+DEPTH_INFO_TOPIC = f'/{args.cam}_camera/camera/aligned_depth_to_color/camera_info'
+print(DEPTH_INFO_TOPIC)
 
 connected_points = {
 'eyeshoulder_left': (1,5),
@@ -48,7 +74,7 @@ thresholds = {
     'torso': 0.5
 }
 
-apply_constraints = ['forearm','upperarm','calf', 'noseye','thigh']
+apply_constraints = ['forearm']
 
 pairs = dict(list(enumerate(openpifpaf.datasets.constants.COCO_KEYPOINTS)))
 pp.pprint(pairs)
@@ -57,19 +83,31 @@ colors = dict()
 for k in range(100):
   colors[k] = tuple(np.random.randint(256, size=3)/256)
 
-import argparse
-parser = argparse.ArgumentParser(description='Visualization options')
-parser.add_argument('--lifetime',
-                 default=1,
-                 help='Marker lifetime')
-parser.add_argument('--filter',
-                 action='store_true',
-                 help='Use filtered poses')
-parser.add_argument('--debug',
-                 action='store_true',
-                 help='Print visualization debug')
 
-args, unknown = parser.parse_known_args()
+
+cameraInfo = rospy.wait_for_message(DEPTH_INFO_TOPIC, CameraInfo, timeout=2)
+logger.info("Got camera info")
+
+def reposition_joint(ref_joint, old_joint, desired_length):
+    x_ref, y_ref, z_ref = ref_joint[:3]
+    x_old, y_old, z_old = old_joint[:3]
+
+    print("Old joint {}".format([x_old, y_old, z_old.real], [x_ref, y_ref, z_ref.real]))
+
+    a = 1
+    b = 2*z_ref
+    c = (x_old - x_ref)**2 + (y_old - y_ref)**2 + z_ref**2 - desired_length**2
+
+    z_new_1 = (-b + np.sqrt(b**2 + 4*a*c+0j))/2*a
+    z_new_2 = (-b + np.sqrt(b**2 - 4*a*c+0j))/2*a
+
+    x = Symbol('x')
+    z_new = float(min(solve((x_old-x_ref)**2 + (y_old-y_ref)**2 + (x-z_ref)**2 - desired_length**2, x)))
+
+    new_joint = [x_old, y_old, z_new]
+
+    print("--> New joint: {} | distance from ref: {} vs. {}".format(new_joint, distance_between_points(new_joint, ref_joint), desired_length) )
+    return new_joint
 
 def pose_cb(msg):
 
@@ -78,25 +116,34 @@ def pose_cb(msg):
 
     for n, skeleton in enumerate(msg.skeletons):
         skeleton_dict = message_converter.convert_ros_message_to_dictionary(skeleton)
+
+        for k, v in skeleton_dict.items():
+            if isinstance(v,list) and len(v):
+                v[:3] = pixel_to_camera(cameraInfo, (v[0],v[1]), v[2])
+
         limbs_before, limbs_after = dict(), dict()
         for limb,conn in connected_points.items():
             label_1 = list(skeleton_dict.keys())[conn[0]]
             label_2 = list(skeleton_dict.keys())[conn[1]]
             if args.debug: logger.debug(f"{label_1} --> {label_2}")
-            pnt_1 = skeleton_dict[label_1]
-            pnt_2 = skeleton_dict[label_2]
+            pnt_1 = skeleton_dict[label_1][:3]
+            pnt_2 = skeleton_dict[label_2][:3]
 
-            label = limb.split('_')[0]
+            if len(pnt_1) and len(pnt_2):
 
-            if len(pnt_1) and len(pnt_2) and label in apply_constraints:
-                distance = distance_between_points(pnt_1, pnt_2)
-                limbs_before[limb] = distance
-                thresh = thresholds[label]
-                if distance > thresh:
-                    pnt_2 = pnt_1 + [thresh * p for p in pnt_2]/np.linalg.norm(pnt_2)
-                    skeleton_dict[list(skeleton_dict.keys())[conn[1]]] = pnt_2
-                limbs_after[limb] = distance_between_points(pnt_1, pnt_2)
-                # print(distance)
+                label = limb.split('_')[0]
+
+                skeleton_dict[list(skeleton_dict.keys())[conn[1]]][:3] = pnt_2
+
+                if label in apply_constraints:
+                    distance = distance_between_points(pnt_1, pnt_2)
+                    limbs_before[limb] = distance
+                    thresh = thresholds[label]
+                    if distance > thresh:
+                        pnt_2 = reposition_joint(ref_joint=pnt_1, old_joint=pnt_2, desired_length=thresh)
+                        skeleton_dict[list(skeleton_dict.keys())[conn[1]]][:3] = pnt_2
+                    limbs_after[limb] = distance_between_points(pnt_1, pnt_2)
+                    # print(distance)
 
             pose.skeletons[n] = message_converter.convert_dictionary_to_ros_message("human_pose_ROS/Skeleton", skeleton_dict)
 
@@ -108,11 +155,11 @@ def pose_cb(msg):
     skel_pub.publish(pose)
 
 
-rospy.init_node('pose_limbs')
 
 
-logger.warning("Using /openpifpaf_pose_transformed")
-pose_sub = rospy.Subscriber('openpifpaf_pose_transformed', PoseEstimation, pose_cb)
+
+logger.warning("Using /openpifpaf_pose")
+pose_sub = rospy.Subscriber('openpifpaf_pose', PoseEstimation, pose_cb)
 limb_before_pub = rospy.Publisher('limbs_before', Limbs, queue_size=100)
 limb_after_pub = rospy.Publisher('limbs_after', Limbs, queue_size=100)
 skel_pub = rospy.Publisher('openpifpaf_pose_constrained_limbs', PoseEstimation, queue_size=100)
