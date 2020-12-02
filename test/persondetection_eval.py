b704d16f4109a283b1f0bcc53e902e92eea93ca2
glhr <grenblot@gmail.com>
2020-12-02 20:03:05 +0100

    running persondetection eval on raw & kalman pose simultaneously

--- test/persondetection_eval.py
+++ test/persondetection_eval.py
@@ -34,16 +34,31 @@ save_points = ["eye", "nose", "shoulder", "elbow", "wrist"]
 
 DEPTH_INFO_TOPIC = '/{}_camera/camera/aligned_depth_to_color/camera_info'.format(args.cam)
 
-num_frames = 0
-num_images = 0
+num_frames = {
+    'kalman': 0,
+    'raw': 0
+}
+num_images = {
+    'kalman': 0,
+    'raw': 0
+}
 
 saving = False
 
-joint_coords = []
+joint_coords = {
+    'kalman': [],
+    'raw': []
+}
 images = []
 
-found_person = 0
-new_image = False
+found_person = {
+    'kalman': 0,
+    'raw': 0
+}
+new_image = {
+    'kalman': False,
+    'raw': False
+}
 
 MAX_UNCERTAINTY = 1
 
@@ -56,78 +71,96 @@ if not os.path.exists(Path.joinpath(project_path,f"action_data")):
     os.makedirs(Path.joinpath(project_path,f"action_data"))
 
 def save_everything():
-    save_images(images)
+    timestamp = time.time()
+    save_images(images, name=timestamp, pose='kalman')
+    save_images(images, name=timestamp, pose='raw')
 
     logger.warning(f"Person found in {found_person} out of {num_frames} frames")
 
-def save_images(images):
-    # with imageio.get_writer(Path.joinpath(project_path,f"test.gif"), mode='I') as writer:
-    #     for i,coords in enumerate(joint_coords):
-    #         font = cv2.FONT_HERSHEY_SIMPLEX
-    #         image = images[i]
-    #         try:
-    #             if len(coords):
-    #                 pt1 = tuple(coords[:2])
-    #                 pt2 = tuple(coords[-2:])
-    #                 print(pt1, pt2)
-    #                 image = cv2.rectangle(image, pt1, pt2, (255,0,0), 2)
-    #         except IndexError as e:
-    #             print(e)
-    #         writer.append_data(image)
-
+def save_images(images, pose='raw', name=time.time()):
     height,width,layers=images[0].shape
     fourcc = cv2.VideoWriter_fourcc(*'H264')
 
-    video=cv2.VideoWriter(f"persondetection-{time.time()}.mp4",fourcc,10,(width,height))
+    video=cv2.VideoWriter(f"persondetection-{name}-{pose}.mp4",fourcc,10,(width,height))
 
-    for i,coords in enumerate(joint_coords):
+    for i,coords in enumerate(joint_coords[pose]):
         font = cv2.FONT_HERSHEY_SIMPLEX
         try:
             image = images[i]
             if len(coords):
-                pt1 = tuple(coords[:2])
-                pt2 = tuple(coords[-2:])
-
-                if args.transform:
+                if pose == 'kalman':
                     pt1 = list(coords[:3])
                     pt2 = list(coords[-3:])
                     pt1 = camera_to_pixel(cameraInfo, pt1)
                     pt2 = camera_to_pixel(cameraInfo, pt2)
+                    color = (0,0,255)
+                else:
+                    pt1 = list(coords[:2])
+                    pt2 = list(coords[-3:-1])
+                    color = (0,255,0)
 
                 pt1 = tuple(int(i) for i in pt1)
                 pt2 = tuple(int(i) for i in pt2)
 
                 print(pt1, pt2)
-                image = cv2.rectangle(image, pt1, pt2, (0,255,0), 2)
-            video.write(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+                image_bb = cv2.rectangle(image.copy(), pt1, pt2, color, 2)
+            video.write(cv2.cvtColor(image_bb, cv2.COLOR_BGR2RGB))
         except IndexError as e:
             print(e)
 
     video.release()
 
-
 def img_cb(msg):
     global images, num_images, new_image
     if not saving:
         # path = msg.data
-        num_images += 1
+        num_images['kalman'] += 1
+        num_images['raw'] += 1
         images.append(image_to_numpy(msg))
-        new_image = True
+        new_image['kalman'] = True
+        new_image['raw'] = True
 
-
-
-def points_cb(msg):
+def points_raw_cb(msg):
     global num_frames, num_images, images, joint_coords, found_person, new_image
 
-    if not saving and new_image:
-        new_image = False
-        num_frames += 1
+    if not saving and new_image['raw']:
+        new_image['raw'] = False
+        num_frames['raw'] += 1
         if len(msg.skeletons):
             skeleton = msg.skeletons[0]
             msg_dict = message_converter.convert_ros_message_to_dictionary(skeleton)
             msg_dict = {k: v for k, v in msg_dict.items() if isinstance(v, list) and k.split("_")[-1] in save_points}
-            msg_dict_tf = dict()
-            pnts = []
+            x = []
+            y = []
+            z = []
+            for i,v in msg_dict.items():
+                if len(v):
+                    uncertainty = v[3:]
+                    x.append(v[0])
+                    y.append(v[1])
+                    z.append(v[2])
+
+            if len(x) and len(y):
+                joint_coords['raw'].append([min(x), min(y),  min(z), max(x), max(y), max(z)])
+                found_person['raw'] += 1
+            else:
+                joint_coords['raw'].append([])
+            print(joint_coords['raw'])
+
+        else:
+            joint_coords['raw'].append([])
+        logger.info(num_frames['raw'])
+
+def points_kalman_cb(msg):
+    global num_frames, num_images, images, joint_coords, found_person, new_image
+
+    if not saving and new_image['kalman']:
+        new_image['kalman'] = False
+        num_frames['kalman'] += 1
+        if len(msg.skeletons):
+            skeleton = msg.skeletons[0]
+            msg_dict = message_converter.convert_ros_message_to_dictionary(skeleton)
+            msg_dict = {k: v for k, v in msg_dict.items() if isinstance(v, list) and k.split("_")[-1] in save_points}
             x = []
             y = []
             z = []
@@ -139,19 +172,19 @@ def points_cb(msg):
                         y.append(v[1])
                         z.append(v[2])
 
-
             if len(x) and len(y):
-                joint_coords.append([min(x), min(y),  min(z), max(x), max(y), max(z)])
-                found_person += 1
+                joint_coords['kalman'].append([min(x), min(y),  min(z), max(x), max(y), max(z)])
+                found_person['kalman'] += 1
             else:
-                joint_coords.append([])
-            print(joint_coords)
+                joint_coords['kalman'].append([])
+            print(joint_coords['kalman'])
 
         else:
-            joint_coords.append([])
-        logger.info(num_frames)
+            joint_coords['kalman'].append([])
+        logger.info(num_frames['kalman'])
 
-pose_sub = rospy.Subscriber('openpifpaf_pose_kalman', PoseEstimation, points_cb)
+pose_kalman_sub = rospy.Subscriber('openpifpaf_pose_kalman', PoseEstimation, points_kalman_cb)
+pose_raw_sub = rospy.Subscriber('openpifpaf_pose', PoseEstimation, points_raw_cb)
 # img_sub = rospy.Subscriber('openpifpaf_savepath', String, img_cb)
 poseimg_sub = rospy.Subscriber('openpifpaf_img', Image, img_cb)
 
